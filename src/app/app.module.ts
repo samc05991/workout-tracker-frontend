6cbcff74024048352812356c7054decca548f8da
Sam Cockburn <samc05991@gmail.com>
2019-04-19 17:29:25 +0100

    feat: remove ng-bootstrap and date-picker component, testing new sass template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 
 import { TokenInterceptor } from './interceptors/token.interceptor';
@@ -15,7 +14,6 @@ import { LoginComponent } from './components/login/login.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { CreateExerciseComponent } from './components/create-exercise/create-exercise.component';
-import { DatePickerComponent } from './components/date-picker/date-picker.component';
 import { CreateWorkoutComponent } from './components/create-workout/create-workout.component';
 import { WorkoutListComponent } from './components/workout-list/workout-list.component';
 
@@ -27,7 +25,6 @@ import { WorkoutListComponent } from './components/workout-list/workout-list.com
     SignUpComponent,
     DashboardComponent,
     CreateExerciseComponent,
-    DatePickerComponent,
     CreateWorkoutComponent,
     WorkoutListComponent,
   ],
@@ -36,7 +33,6 @@ import { WorkoutListComponent } from './components/workout-list/workout-list.com
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    NgbModule
   ],
   providers: [
     {
